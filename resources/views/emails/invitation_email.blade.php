2d1e8f4fa92be578783971ba666ca19d6d51b574
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2024-12-24 17:38:31 +0200

    Update invitation_email.blade.php

--- resources/views/emails/invitation_email.blade.php
+++ resources/views/emails/invitation_email.blade.php
@@ -3,12 +3,42 @@
 <head>
     <title>دعوة دوري المناظرات</title>
 </head>
-<body>
-<div style="text-align: center; margin-bottom: 20px;">
-    <img src="{{ asset('images/public.png') }}" class="img-fluid zoom-image" alt="">
+
+<body style="direction: rtl;">
+<div style="text-align: center !important;">
+    <img src="{{ asset('images/public.png') }}" style="height: 200px !important; object-fit: cover !important;"  alt="">
 </div>
 
-<h1 style="text-align: right;"> {{ $invitation['first_name'] }} أهلا </h1>
-<p style="text-align: center; font-size: 1rem;">شكرا لتسجيلك معنا</p>
+<h1 style="text-align: right; margin-top: 0"> أهلا {{ $invitation['first_name'] }}  </h1>
+<p style="text-align: center !important; font-size: 1rem !important;">شكرًا لتسجيلكم لحضور دوري مناظرات وزارة الرياضة</p>
+
+<p style="text-align: center !important; font-size: 1rem !important;">نشكر لكم اهتمامكم وتسجيلكم لحضور برنامج دوري مناظرات وزارة الرياضة.</p>
+
+<p style="text-align: center !important; font-size: 1rem !important;">نتطلع لاستقبالكم في الحدث والاستمتاع معًا بمنافسات حوارية مميزة تعكس روح
+    التفكير النقدي والحوار البنّاء.</p>
+
+<p style="text-align: left; font-size: 1rem;">نراكم قريبًا!</p>
+
+<div style="text-align: center !important;">
+    <button style="display: inline-block;
+    padding: 0.5rem 1rem;
+    font-size: 1.25rem;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #fff;
+    text-align: center;
+    text-decoration: none;
+    vertical-align: middle;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+    border: 1px solid #0d6efd;
+    border-radius: 0.5rem;
+    background-color: #0d6efd;
+    transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+">رابط الدعوة</button>
+</div>
+
 </body>
 </html>
