225a705f10e15c99f9d94d63171a7233237b0633
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2024-12-25 01:12:44 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('register')->group(function () {
+Route::prefix('attend')->group(function () {
     Route::get('/', [InvitationController::class, 'index'])->name('visitor-invitation-form');
 
     Route::post('/', [InvitationController::class, 'store'])->name('visitor-invitation-submit');
@@ -34,7 +34,7 @@ Route::prefix('register')->group(function () {
 });
 
 
-Route::prefix('attend')->group(function () {
+Route::prefix('register')->group(function () {
     Route::get('/', [StudentController::class, 'index'])->name('team-invitation-form');
 
     Route::post('/', [StudentController::class, 'store'])->name('team-invitation-submit');
