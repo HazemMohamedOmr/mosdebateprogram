f817b584c0dcbd479e556d5fd02ca4220c7d97c7
Sameh Mohamed Ahmed Omr <Conan_Sameh@yahoo.com>
2024-12-24 19:49:08 +0200

    Update qrcode.blade.php

--- resources/views/invitation/qrcode.blade.php
+++ resources/views/invitation/qrcode.blade.php
@@ -6,13 +6,15 @@
         <div class="form-container mx-auto" style="max-width: 700px;">
             <div class="my-5 separator">
                 <div class="row">
-                    <div class="col-12 order-1 order-md-2">
-                        <img src="{{ asset('images/public.png') }}" class="img-fluid zoom-image">
+                    <div class="col-12 order-1 order-md-2 d-flex justify-content-center">
+                        <img src="{{ asset('images/public.png') }}" class="img-fluid zoom-image" style="height: 250px;">
                     </div>
                 </div>
             </div>
-            
-            <img src="{{ $image }}" alt="">
+
+           <div class="d-flex justify-content-center">
+               {{ $image }}
+           </div>
 
         </div>
     </div>
